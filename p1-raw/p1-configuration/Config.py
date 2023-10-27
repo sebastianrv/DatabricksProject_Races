96866e546f5ef4e989e15952590788e2c33330e3
sebastianrv <sebastianrv>
2023-10-27 01:23:32 +0000

    acceso Sebas

--- p1-raw/p1-configuration/Config.py
+++ p1-raw/p1-configuration/Config.py
@@ -4,11 +4,11 @@
 
 # COMMAND ----------
 
-storageAccountName = "sagrupo3proyecto1"
-storageAccountAccessKey = "dCqFrsx23kMp40BMATH/iyWqyHQ396vVGDMkh0M0YhKOSnguPHMXWx81FLRdrTjbqPlGM5wZjhTB+AStT8JcMw=="
-sasToken = "?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2023-11-02T11:48:38Z&st=2023-10-26T03:48:38Z&spr=https&sig=O82lWvvnr2Q9xKE8Cmv3PhrLk%2Bj19EzVjVRcj29DpPk%3D"
+storageAccountName = "sarsv2023"
+storageAccountAccessKey = "4laHHRo3eLqlGttXboMIODFDxL85I+F8AitgF5cPS2HB+2CfzW8Wh8PKzscgVan/CFG8/i5unlWk+AStABkbNA=="
+sasToken = "?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2023-11-01T09:18:19Z&st=2023-10-27T01:18:19Z&spr=https&sig=Nbn2QYz0si6T7MNzx81UraySa4JGN9W57yGlc3oliEw%3D"
 blobContainerName = "p1-raw"
-mountPoint = "/mnt/sagrupo3proyecto1/p1-raw"
+mountPoint = "/mnt/sarsv2023/p1-raw"
 if not any(mount.mountPoint == mountPoint for mount in dbutils.fs.mounts()):
   try:
     dbutils.fs.mount(
